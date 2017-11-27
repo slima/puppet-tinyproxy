fd2b7dc25cf056792da071453567b85040ac4d44
Puppet <kamil@4web.pl>
2017-11-27 18:07:54 +0100

    service only if tinyproxy::manage_service == true

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,7 +1,10 @@
 class tinyproxy::service inherits tinyproxy {
-  service { $tinyproxy::service_name:
-    ensure    => running,
-    subscribe => Package['tinyproxy'],
-    require   => Class['tinyproxy::install'],
+
+  if tinyproxy::manage_service == true {
+    service { $tinyproxy::service_name:
+      ensure    => running,
+      subscribe => Package['tinyproxy'],
+      require   => Class['tinyproxy::install'],
+    }
   }
 }
